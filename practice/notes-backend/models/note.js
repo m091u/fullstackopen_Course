3875f34abf66a1696417dc06d0ad056c4416e72c
m091u <mirabelatiugan@gmail.com>
2024-02-17 15:10:31 +0100

    part 4a

--- practice/notes-backend/models/note.js
+++ practice/notes-backend/models/note.js
@@ -1,19 +1,4 @@
-const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const mongoose = require("mongoose")
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -22,14 +7,14 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
+})
 
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model("Note", noteSchema)
