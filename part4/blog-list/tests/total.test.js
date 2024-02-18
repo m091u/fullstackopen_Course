e66507e4cb2c4cd2202695286437eebe31b6e4f9
m091u <mirabelatiugan@gmail.com>
2024-02-18 17:24:24 +0100

    part4a - testing node apps

---
+++ part4/blog-list/tests/total.test.js
@@ -0,0 +1,69 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
+const { totalLikes } = require('../utils/list_helper');
+
+describe('total likes', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ];
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = totalLikes([blogs[0]]);
+    assert.strictEqual(result, 5);
+  });
+
+  test('when list has more than one blog, equals the sum of likes of all', () => {
+    const result = totalLikes(blogs);
+    assert.strictEqual(result, 29);
+  });
+
+  test('when list has only one blog with 0 likes, equals 0', () => {
+    const result = totalLikes([{ likes: 0 }]);
+    assert.strictEqual(result, 0);
+  });
+
+  test('when list has only one blog with more than 0 likes, equals the likes of that', () => {
+    const result = totalLikes([blogs[2]]);
+    assert.strictEqual(result, 10);
+  });
+});
