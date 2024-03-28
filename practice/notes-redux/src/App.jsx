843bb8ed9acffebb22914ec4bccd0ff6874f2054
m091u <mirabelatiugan@gmail.com>
2024-03-28 15:48:15 +0100

    6a redux

---
+++ practice/notes-redux/src/App.jsx
@@ -0,0 +1,14 @@
+import NewNote from "./newNote";
+import Notes from "./Notes";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux Note App</h1>
+      <NewNote />
+      <Notes />
+    </div>
+  );
+};
+
+export default App;
