76c9cc31a16c31c3915c264e5972c9ae297720c4
m091u <mirabelatiugan@gmail.com>
2024-04-01 09:31:21 +0200

    6c Redux & server

--- practice/notes-redux/src/main.jsx
+++ practice/notes-redux/src/main.jsx
@@ -5,8 +5,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from "react-redux";
 import App from "./App";
 
-import noteReducer from "./reducers/noteReducer";
 import filterReducer from './reducers/filterReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 
 const store = configureStore({
   reducer: {
@@ -15,7 +15,6 @@ const store = configureStore({
   }
 })
 
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
